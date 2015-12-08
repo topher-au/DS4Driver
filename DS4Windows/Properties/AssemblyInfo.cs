08072b6fa6308c551f4fb5d123e8e508d91df8d2
Topher Sheridan <topher@xobanimot.com>
2015-12-09 01:51:32 +1100

    Initial commit

--- DS4Windows/Properties/AssemblyInfo.cs
+++ DS4Windows/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a52b5b20-d9ee-4f32-8518-307fa14aa0c6")]
+[assembly: Guid("a52b5b20-dead-beef-cafe-307fa14aa0c6")]
 
 // Version information for an assembly consists of the following four values:
 //
